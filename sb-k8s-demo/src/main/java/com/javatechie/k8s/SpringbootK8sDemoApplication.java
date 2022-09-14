797146f115a197624411b156c59e43ccc70a2e55
Chandra Udatha <csudatha@gmail.com>
2022-09-14 16:45:37 -0700

    triggering build
    triggering build

--- sb-k8s-demo/src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ sb-k8s-demo/src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootK8sDemoApplication {
 
 	@GetMapping("/message")
 	public String displayMessage(){
-		return "Congratulation. Your first successfull K8S app is running. Chandra Udatha !!";
+		return "Congratulation. Your first successfull K8S app is running. Chandra Udatha !! \n triggered a auto build";
 	}
 
 	public static void main(String[] args) {
